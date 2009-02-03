b59e3f800eb6fdcf2baffbfa1c7c5f31f0370a4f
glurbi <glurbi@gmail.com>
2009-02-03 11:51:01 +0100

    prints ... for the blob values in BOMDataTreeModel

--- src/main/scala/bom/ui/BOMDataTreeModel.scala
+++ src/main/scala/bom/ui/BOMDataTreeModel.scala
@@ -10,7 +10,7 @@ class BOMDataTreeModel(val doc: BOMDocument) extends AbstractTreeTableModel(doc)
 
   def getValueAt(node: Object, column: Int): Object = column match {
     case 0 => node.asInstanceOf[BOMNode].name
-    case 1 => translateValue(node.asInstanceOf[BOMNode].value)
+    case 1 => translateValue(node.asInstanceOf[BOMNode])
     case 2 => "" + node.asInstanceOf[BOMNode].position / 8
     case 3 => translateSchema(node.asInstanceOf[BOMNode].schema)
   }
@@ -26,10 +26,11 @@ class BOMDataTreeModel(val doc: BOMDocument) extends AbstractTreeTableModel(doc)
 
   override def isLeaf(node: Object): Boolean = node.isInstanceOf[BOMLeaf]
 
-  private def translateValue(value: Any): String =
-    value match {
-      case null => ""
-      case _ => value.toString
+  private def translateValue(node: BOMNode): String =
+    node match {
+      case BOMBlob(_, _, _, _) => "..."
+      case BOMContainer(_, _, _) => ""
+      case _ => node.value.toString
     }
 
   private def translateSchema(schema: BOMSchemaElement): String = schema match {
