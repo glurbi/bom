572dc19bdd41cdd3d95c94d8fab2f0b937178eb9
glurbi <glurbi@gmail.com>
2009-06-26 17:27:35 +0200

    replace java.lang.HashMap with scala.Map in SchemaLeaf

--- src/main/scala/bom/schema/SchemaLeaf.scala
+++ src/main/scala/bom/schema/SchemaLeaf.scala
@@ -1,24 +1,20 @@
 package bom.schema
 
-import java.util.{HashMap => JHashMap}
-
 abstract case class SchemaLeaf() extends SchemaElement {
 
   // using String makes sure we have a 'natural' behavior when comparing objects
-  // of different classes that represent the same value (1 Integer and 1 Long for instance)
-  private val mappings = new JHashMap[String, AnyRef]
-  
+  // of different classes that represent the same value (Integer and Long for instance)
+  private val mappings = scala.collection.mutable.Map.empty[String, AnyRef]
+
   var defaultMapping: AnyRef = null
 
-  def hasMapping: Boolean = mappings.size() > 0
+  def hasMapping: Boolean = mappings.size > 0
 
   def addMapping(value: Any, mappedValue: Object) {
-    mappings.put(value.toString, mappedValue)
+    mappings += value.toString -> mappedValue
   }
 
-  def mappedValue(value: Any): AnyRef = {
-    val o = mappings.get(value.toString)
-    if (o != null) o else defaultMapping
-  }
+  def mappedValue(value: Any): AnyRef =
+    mappings.getOrElse(value.toString, defaultMapping)
   
 }
