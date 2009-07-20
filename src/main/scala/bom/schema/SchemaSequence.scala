2f61759b363706ec7be3c1b540b588401dd63adb
glurbi <glurbi@gmail.com>
2009-07-20 15:33:14 +0200

    less java-ish, more scala-ish

--- src/main/scala/bom/schema/SchemaSequence.scala
+++ src/main/scala/bom/schema/SchemaSequence.scala
@@ -1,8 +1,9 @@
 package bom.schema
 
-import java.util.{HashMap => JHashMap}
 import bom._
 import bom.bin._
+
+import scala.collection.mutable.HashMap
 
 case class SchemaSequence(
   override val name:String,
@@ -11,21 +12,23 @@ case class SchemaSequence(
 extends SchemaElement {
 
   private var elements: List[SchemaElement] = Nil
-  val name2element = new JHashMap[String, SchemaElement]
-  val name2index = new JHashMap[String, Int]
+  var name2element = HashMap[String, SchemaElement]()
+  var name2index = HashMap[String, Int]()
 
   override def add(child: SchemaElement) {
     elements = elements ::: List(child)
-    name2element.put(child.name, child)
-    name2index.put(child.name, children.size - 1);
+    if (child.name != null) {
+      name2element += child.name -> child
+      name2index += child.name -> (children.size - 1)
+    }
   }
 
   override def createNode(parent: BOMContainer, index: Int): BOMNode =
     new BOMSequence(this, parent, index)
 
-  def child(name: String): SchemaElement = name2element.get(name)
+  def child(name: String): SchemaElement = name2element.getOrElse(name, null)
 
-  def childIndex(name: String): Int = name2index.get(name)
+  def childIndex(name: String): Int = name2index(name)
 
   def children: List[SchemaElement] = elements
 
